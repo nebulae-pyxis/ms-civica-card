f33b7be1f79729b2be3dd3d70fce6e0f2bca82b4
Daniel Tobon <daniel.tobon@nebulae.com.co>
2018-11-21 16:07:50 -0500

    Se modifica el nombre del module

--- frontend/civica-card-reload-wc/src/main.ts
+++ frontend/civica-card-reload-wc/src/main.ts
@@ -2,13 +2,13 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import 'hammerjs';
-import { AppModule } from './app/app.module';
+import { WebComponentModule } from './app/web-component.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(WebComponentModule)
   .catch(err => console.error(err));
 
