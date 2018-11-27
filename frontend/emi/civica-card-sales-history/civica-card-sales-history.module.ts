92f2939270bf7662a8c9eb8bf103bff74940c9ed
Jaime Zapata <esteban.zapata@nebulae.com.co>
2018-11-27 11:05:54 -0500

    Adds new page to show the civica card reload history

--- frontend/emi/civica-card-sales-history/civica-card-sales-history.module.ts
+++ frontend/emi/civica-card-sales-history/civica-card-sales-history.module.ts
@@ -1,3 +1,4 @@
+import { CivicaCardSalesHistoryService } from './civica-card-sales-history.service';
 import { SaleHistoryService } from './sales-history/sale-history.service';
 import { SaleHistoryComponent } from './sales-history/sale-history.component';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
@@ -21,7 +22,7 @@ const routes: Routes = [
   declarations: [
     SaleHistoryComponent
   ],
-  providers: [SaleHistoryService],
+  providers: [CivicaCardSalesHistoryService, SaleHistoryService],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
 })
 
--- playground/generate_playground.sh
+++ playground/generate_playground.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # FrontEnd - EMI composition
-nebulae compose-ui development --shell-type=FUSE2_ANGULAR --shell-repo=https://github.com/nebulae-pyxis/emi --frontend-id=emi --output-dir=/home/nesas-02/Projects/PYXIS/ms-civica-card/playground/emi  --setup-file=/home/nesas-02/Projects/PYXIS/ms-civica-card/etc/mfe-setup.json,/home/nesas-02/Projects/PYXIS/ms-business-management/etc/mfe-setup.json
+nebulae compose-ui development --shell-type=FUSE2_ANGULAR --shell-repo=https://github.com/nebulae-pyxis/emi --frontend-id=emi --output-dir=emi  --setup-file=../etc/mfe-setup.json
 
-# API - GateWay composition
-nebulae compose-api development --api-type=NEBULAE_GATEWAY --api-repo=https://github.com/nebulae-pyxis/sales-gateway --api-id=sales-gateway --output-dir=/home/nesas-02/Projects/PYXIS/ms-civica-card/playground/sales-gateway  --setup-file=/home/nesas-02/Projects/PYXIS/ms-civica-card/etc/mapi-setup.json
+# API - SALES GateWay composition
+nebulae compose-api development --api-type=NEBULAE_GATEWAY --api-repo=https://github.com/nebulae-pyxis/sales-gateway --api-id=sales-gateway --output-dir=sales-gateway  --setup-file=../etc/mapi-setup.json
+
+# API - EMI GateWay composition
+nebulae compose-api development --api-type=NEBULAE_GATEWAY --api-repo=https://github.com/nebulae-pyxis/emi-gateway --api-id=emi-gateway --output-dir=emi-gateway  --setup-file=../etc/emi-mapi-setup.json
